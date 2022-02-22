9efb49285a71102727bdc50388cb9476ad96b0e3
Martynov Maksim <martynmaks@gmail.com>
2022-02-22 19:21:35 +0300

    initialize project

---
+++ app/src/main/java/ru/androidschool/intensiv/MainActivity.kt
@@ -0,0 +1,39 @@
+package ru.androidschool.intensiv
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import ru.androidschool.intensiv.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+        private val API_KEY = BuildConfig.THE_MOVIE_DATABASE_API
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("CheckResult")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
+
+        // Set up Action Bar
+        val navController = host.navController
+
+        setupBottomNavMenu(navController)
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+}

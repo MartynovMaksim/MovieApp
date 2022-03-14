e6842852a4f164c12d9b406efc36f5fe8c8a3f2e
Martynov Maksim <martynmaks@gmail.com>
2022-03-14 22:07:46 +0300

    create consts for params

--- app/src/main/java/ru/androidschool/intensiv/network/MovieApiInterface.kt
+++ app/src/main/java/ru/androidschool/intensiv/network/MovieApiInterface.kt
@@ -7,39 +7,45 @@ import ru.androidschool.intensiv.MainActivity
 import ru.androidschool.intensiv.data.MoviesResponse
 import ru.androidschool.intensiv.data.TvShowsResponse
 
+object consts {
+    const val language: String = "ru"
+    const val pages: Int = 1
+}
+
 interface MovieApiInterface {
+
     @GET("movie/now_playing")
     fun getNowPlayingMovies(
         @Query("api_key") apiKey: String = MainActivity.API_KEY,
-        @Query("language") language: String = "ru",
-        @Query("page") page: Int = 1
+        @Query("language") language: String = consts.language,
+        @Query("page") page: Int = consts.pages
     ): Single<MoviesResponse>
 
     @GET("movie/upcoming")
     fun getUpcomingMovies(
         @Query("api_key") apiKey: String = MainActivity.API_KEY,
-        @Query("language") language: String = "ru",
-        @Query("page") page: Int = 1
+        @Query("language") language: String = consts.language,
+        @Query("page") page: Int = consts.pages
     ): Single<MoviesResponse>
 
     @GET("movie/popular")
     fun getPopularMovies(
         @Query("api_key") apiKey: String = MainActivity.API_KEY,
-        @Query("language") language: String = "ru",
-        @Query("page") page: Int = 1
+        @Query("language") language: String = consts.language,
+        @Query("page") page: Int = consts.pages
     ): Single<MoviesResponse>
 
     @GET("tv/popular")
     fun getPopularTvShow(
         @Query("api_key") apiKey: String = MainActivity.API_KEY,
-        @Query("language") language: String = "ru",
-        @Query("page") page: Int = 1
+        @Query("language") language: String = consts.language,
+        @Query("page") page: Int = consts.pages
     ): Single<TvShowsResponse>
 
     @GET("search/movie")
     fun getSearchResult(
         @Query("api_key") apiKey: String = MainActivity.API_KEY,
         @Query("query") query: String,
-        @Query("language") language: String = "ru"
+        @Query("language") language: String = consts.language
     ): Single<MoviesResponse>
 }
